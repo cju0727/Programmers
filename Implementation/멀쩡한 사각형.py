db38de7c28437d91a805e57dc98ed0023fba7d46
cju0727 <chojw0727@gmail.com>
2022-06-19 21:07:00 +0900

    Upload Implementation_멀쩡한 사각형

---
+++ Implementation/멀쩡한 사각형.py
@@ -0,0 +1,15 @@
+def solution(w,h):
+    answer = 1
+    gcd = 0
+    
+    for i in range(min(w,h), 0, -1):
+        if w%i == 0 and h%i == 0:
+            gcd = i
+            break
+    
+    answer = w*h - (w+h-gcd)
+    return answer
+
+w = 8
+h = 12
+print(solution(w, h))
